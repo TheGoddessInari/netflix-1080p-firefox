d104fb40c5c54362975273292fb6a2379999a445
TheGoddessInari <thegoddessinari@gmail.com>
2020-06-08 18:22:51 -0700

    Add HQ stereo audio.

--- src/background.js
+++ src/background.js
@@ -10,7 +10,7 @@ browser.webRequest.onBeforeRequest.addListener(
           // Version 6.0023.745.051 from https://assets.nflxext.com/en_us/ffe/player/html/cadmium-playercore-6.0023.745.051.js
           if (text.includes(`this.version="6.0023.745.051";`)) {
             // Use our profile list
-            text = text.replace(`q={type:"standard",viewableId:p,profiles:m,`, `profiles=["playready-h264mpl30-dash","playready-h264mpl31-dash","playready-h264mpl40-dash","playready-h264hpl30-dash","playready-h264hpl31-dash","playready-h264hpl40-dash","vp9-profile0-L30-dash-cenc","vp9-profile0-L31-dash-cenc","vp9-profile0-L40-dash-cenc","heaac-2-dash","simplesdh","nflx-cmisc","BIF240","BIF320"],use6Channels&&profiles.push("heaac-5.1-dash"),q={type:"standard",viewableId:p,profiles:profiles,`);
+            text = text.replace(`q={type:"standard",viewableId:p,profiles:m,`, `profiles=["playready-h264mpl30-dash","playready-h264mpl31-dash","playready-h264mpl40-dash","playready-h264hpl30-dash","playready-h264hpl31-dash","playready-h264hpl40-dash","vp9-profile0-L30-dash-cenc","vp9-profile0-L31-dash-cenc","vp9-profile0-L40-dash-cenc","heaac-2-dash","heaac-2hq-dash","simplesdh","nflx-cmisc","BIF240","BIF320"],use6Channels&&profiles.push("heaac-5.1-dash"),q={type:"standard",viewableId:p,profiles:profiles,`);
             // Re-enable Ctrl-Shift-S menu
             text = text.replace(`this.tY.xga&&this.toggle()`, `this.toggle();`);
             // Add Audio Format Description
