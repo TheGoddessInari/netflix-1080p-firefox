1d3eb13654568f3f6183fdbe66a413866714aa33
TheGoddessInari <thegoddessinari@gmail.com>
2020-06-08 17:21:03 -0700

    Upgrade to cadmium playercore 6.0023.745.051. Removed deprecated functions.

--- src/background.js
+++ src/background.js
@@ -7,26 +7,14 @@ browser.webRequest.onBeforeRequest.addListener(
       fetch(browser.extension.getURL("cadmium-playercore.js")).
         then(response => response.text()).
         then(text => {
-          // Version 6.0023.327.011 from https://assets.nflxext.com/en_us/ffe/player/html/cadmium-playercore-6.0023.327.011.js
-          if (text.includes(`this.version="6.0023.327.011";`)) {
-            // Add global variable for overriding manifest
-            // text = `var manifestOverridden=false;${text}`;
-            // Unconditionally push 1080p formats
-            text = text.replace(`b.KH=[y.V.gI,y.V.CC,y.V.GQ,y.V.HQ,y.V.cJ,y.V.dJ]`, `b.KH=[y.V.gI,y.V.CC,y.V.GQ,y.V.HQ,y.V.cJ,y.V.dJ,y.V.JQ, y.V.IQ, y.V.eJ]`);
+          // Version 6.0023.745.051 from https://assets.nflxext.com/en_us/ffe/player/html/cadmium-playercore-6.0023.745.051.js
+          if (text.includes(`this.version="6.0023.745.051";`)) {
             // Use our profile list
-            text = text.replace(`g={type:"standard",viewableId:h,profiles:p,`, `profiles=["playready-h264mpl30-dash","playready-h264mpl31-dash","playready-h264mpl40-dash","playready-h264hpl30-dash","playready-h264hpl31-dash","playready-h264hpl40-dash","vp9-profile0-L30-dash-cenc","vp9-profile0-L31-dash-cenc","vp9-profile0-L40-dash-cenc","heaac-2-dash","simplesdh","nflx-cmisc","BIF240","BIF320"],use6Channels&&profiles.push("heaac-5.1-dash"),g={type:"standard",viewableId:h,profiles:profiles,`);
+            text = text.replace(`q={type:"standard",viewableId:p,profiles:m,`, `profiles=["playready-h264mpl30-dash","playready-h264mpl31-dash","playready-h264mpl40-dash","playready-h264hpl30-dash","playready-h264hpl31-dash","playready-h264hpl40-dash","vp9-profile0-L30-dash-cenc","vp9-profile0-L31-dash-cenc","vp9-profile0-L40-dash-cenc","heaac-2-dash","simplesdh","nflx-cmisc","BIF240","BIF320"],use6Channels&&profiles.push("heaac-5.1-dash"),q={type:"standard",viewableId:p,profiles:profiles,`);
             // Re-enable Ctrl-Shift-S menu
-            text = text.replace(`this.aX.pca&&this.toggle()`, `this.toggle();`);
-            // Replace w/ Windows Edge esnPrefix
-            // text = text.replace(`/Windows NT/.test(a)?"NFCDCH-02-"`, `/Windows NT/.test(a)?"NFCDIE-03-"`);
-            // Replace w/ MacOS Edge esnPrefix
-            // text = text.replace(`/Intel Mac OS X/.test(a)?"NFCDCH-MC-"`, `/Intel Mac OS X/.test(a)?"NFCDIE-04-"`);
+            text = text.replace(`this.tY.xga&&this.toggle()`, `this.toggle();`);
             // Add Audio Format Description
-            text = text.replace(`displayName:a.ZX`, `displayName:a.ZX+" - "+a.channelsFormat`);
-            // Add async to this function so we can await in the next part.
-            // text = text.replace(`X6a(c++,a,b).then(function(d)`, `X6a(c++,a,b).then(async function(d)`);
-            // Get manifest - await
-            // text = text.replace(`if(void 0===d.result)`, `if(/watch/.test(window.location.pathname)&&d.result.from.startsWith("nq_cadmium_pbo_manifests")&&!manifestOverridden){console.log("Manifest locked to Edge (or not available in 1080p)"),console.log("Getting Edge manifest");var manifest=await getManifest("NFCDIE-03-"+generateEsn());console.log("Acquisition successful, commence playback"),manifest.playbackContextId=d.result.result.playbackContextId,d.result.result=manifest,manifestOverridden=true}if(void 0===d.result)`)
+            text = text.replace(`displayName:a.yZ`, `displayName:a.yZ+" - "+a.channelsFormat`);
           }
           filter.write(encoder.encode(text));
           filter.close();
