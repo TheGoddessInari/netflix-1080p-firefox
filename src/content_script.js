8272c9e061ff84bcebacdcbc8a28a88479ee4cad
TheGoddessInari <thegoddessinari@gmail.com>
2021-10-18 17:56:05 -0700

    Constify content_script.js.

--- src/content_script.js
+++ src/content_script.js
@@ -1,8 +1,5 @@
 // From EME Logger extension
 
-urls = [
-    'netflix_max_bitrate.js',
-];
 
 // very messy workaround for accessing browser's storage outside of background / content scripts
 browser.storage.sync.get({
@@ -10,33 +7,36 @@ browser.storage.sync.get({
     setMaxBitrate: false,
     useVP9: true
 }).then(items => {
-    var use6Channels = items.use6Channels;
-    var setMaxBitrate = items.setMaxBitrate;
-    var useVP9 = items.useVP9;
-    var mainScript = document.createElement('script');
-    mainScript.type = 'application/javascript';
-    mainScript.text = `var use6Channels = ${use6Channels};
+    const include_urls = [
+        'netflix_max_bitrate.js',
+    ];
+    const use6Channels = items.use6Channels;
+    const setMaxBitrate = items.setMaxBitrate;
+    const useVP9 = items.useVP9;
+    const main_script = document.createElement('script');
+    main_script.type = 'application/javascript';
+    main_script.text = `var use6Channels = ${use6Channels};
                     var setMaxBitrate = ${setMaxBitrate};
                     var useVP9 = ${useVP9};`;
-    document.documentElement.appendChild(mainScript);
+    document.documentElement.appendChild(main_script);
 
-    for (var i = 0; i < urls.length; i++) {
-        var mainScriptUrl = browser.extension.getURL(urls[i]);
+    for (const include_url of include_urls) {
+        const secondary_url = browser.extension.getURL(include_url);
 
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', mainScriptUrl, true);
+        const include_xhr = new XMLHttpRequest();
+        include_xhr.open('GET', secondary_url, true);
 
-        xhr.onload = function(e) {
-            var xhr = e.target;
-            var mainScript = document.createElement('script');
-            mainScript.type = 'application/javascript';
-            if (xhr.status == 200) {
-                mainScript.text = xhr.responseText;
-                document.documentElement.appendChild(mainScript);
+        include_xhr.onload = function(e) {
+            const onload_xhr = e.target;
+            if (onload_xhr.status == 200) {
+                const xhr_script = document.createElement('script');
+                xhr_script.type = 'application/javascript';
+                    xhr_script.text = onload_xhr.responseText;
+                document.documentElement.appendChild(xhr_script);
             }
         };
 
-        xhr.send();
+        include_xhr.send();
     }
     return;
 }).catch(err => {
